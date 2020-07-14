90430bb4969426211ba5984d66168a164c023626
JH <18468107890@163.com>
2020-07-14 13:46:49 +0800

    deploy

---
+++ precache-manifest.cc5bed184e44284cf0f43c5dda7c3b71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb254110e6570797bf85741a04d37b01",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "d51e7532c2422446789e",
+    "url": "/money-website/static/css/main.9b20cde8.chunk.css"
+  },
+  {
+    "revision": "0bf6781c71ea22cb4f5d",
+    "url": "/money-website/static/js/2.0d5038e7.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/money-website/static/js/2.0d5038e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51e7532c2422446789e",
+    "url": "/money-website/static/js/main.48cb58df.chunk.js"
+  },
+  {
+    "revision": "675cdaee68cd4508492f",
+    "url": "/money-website/static/js/runtime-main.e4eea04f.js"
+  }
+]);
